b6e16017734da082e889f4981446577409e7eb77
Cedric <167123360+Cedric0915@users.noreply.github.com>
2024-06-27 12:45:22 +0800

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<style>
+.icons img:hover
+{
+    transform: scale(1.2);
+    transition: transform 0.5s ease;
+}
+</style>
+
+<div class="header">
+    <a href="/eatlemou" class="logo"><span>Eat Le Mou</span>.Co</a>
+
+    <nav class="navbar">
+        <a id="about" class="option" href="/eatlemou/about/aboutus" title="About Eat Le Mou">ABOUT</a>
+        <a id="menu" class="option" href="/eatlemou/menu" title="Menu">MENU</a>
+        <a id="event" class="option" href="/eatlemou/promotion" title="Exclusive Promotions">PROMOTION EVENTS</a>
+        <a id="contact" class="option" href="/eatlemou/contactus">CONTACT US</a>
+    </nav>
+
+    <div class="icons">
+        <a href="/eatlemou/loginorregister/index.php" class="icon"><img src="/eatlemou/image/user.png" alt="user icon" title="Log in/Register"></a>
+    </div>
+</div>
